75e1347ea9141a0e266eab7b6b56f02f9a64c011
noparamin <noparamin@naver.com>
2022-07-06 15:52:05 +0900

    관심사 분리 AppConfig 생성, 정률할인정책 구현

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -11,8 +11,10 @@ import hello.core.order.OrderServiceImpl;
 public class OrderApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
@@ -21,7 +23,6 @@ public class OrderApp {
         Order order = orderService.createOrder(memberId, "itemA", 10000);
 
         System.out.println("order = " + order.toString());
-
 
     }
 }
