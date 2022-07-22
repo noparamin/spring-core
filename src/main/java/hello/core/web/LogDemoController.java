cfd5dc415808672f3a5ee1b479eb527e873223eb
noparamin <noparamin@naver.com>
2022-07-22 19:35:37 +0900

    객체 조회 지연처리

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -15,6 +15,7 @@ public class LogDemoController {
 
     private final LogDemoService logDemoService;
     //private final ObjectProvider<MyLogger> myLoggerProvider;
+    // 의존관계 주입을 지연시키 위해 ObjectProvider를 썼지만 프록시를 MyLogger에 설정하면 에러나지 않고 MyLogger 그대로 사용 가능
     private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
