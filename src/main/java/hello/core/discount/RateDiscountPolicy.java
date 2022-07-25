b567af702d06930a39e64232369134014303c9df
noparamin <noparamin@naver.com>
2022-07-25 22:47:40 +0900

    Primary와 Qualifier 정리

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-//@Primary
+//@Primary 같은 타입의 빈이 여러 개 조회 될 때 해결방법 중 하나, 메인으로 사용하는 것에 @Primary를 붙여 적용하고 서브에는 @Qualifier를 지정하는 것이 좋다
 @MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy {
 
