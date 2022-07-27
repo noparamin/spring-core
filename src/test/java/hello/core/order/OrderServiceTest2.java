3a172571daa12bab28fccf5421f68cc949079002
noparamin <noparamin@naver.com>
2022-07-27 15:25:24 +0900

    OrderServiceTest2 단위테스트로 수정

--- src/test/java/hello/core/order/OrderServiceTest2.java
+++ src/test/java/hello/core/order/OrderServiceTest2.java
@@ -1,25 +1,22 @@
 package hello.core.order;
 
-import hello.core.AppConfig;
+import hello.core.AutoAppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-@SpringBootTest
 public class OrderServiceTest2 {
-
-    @Autowired
-    MemberService memberService;
-    @Autowired
-    OrderService orderService;
 
     @Test
     void createOrder() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+        MemberService memberService = ac.getBean(MemberService.class);
+        OrderService orderService = ac.getBean(OrderService.class);
+
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
